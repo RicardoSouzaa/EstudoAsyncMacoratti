5c0a9b854312cb47dae43acacc143b7b35e29d41
RIcardo Souza <ryszard696@yahoo.com.br>
2022-11-18 13:32:02 -0300

    first

---
+++ Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TesteWfAsyncMacoratti
+{
+    public partial class Form1 : Form
+    {
+        int cliques = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            // primeira task que executa
+            Console.WriteLine("Evento do botão foi iniciado : Cliqui aqui");
+
+            //executa o longa --- primeiro verifica bisexto, espera 10 segs e finalizar
+            Teste teste = new Teste();
+            await teste.Task_LongaDuracao();
+
+            // apoós o fim do task longo ele imprime
+            Console.WriteLine("Evento do botão foi concluído");
+
+            Console.WriteLine($"você clicou {++cliques} vezes ------------ {DateTime.Now} ");
+        }
+    }
+}
---
+++ Teste.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteWfAsyncMacoratti
+{
+    class Teste
+    {
+        public async Task<bool> Task_Tresult_Async()
+        {
+            return await Task.FromResult<bool>(DateTime.IsLeapYear(DateTime.Now.Year));
+        }
+
+        public async Task Task_Void_Async()
+        {
+            await Task.Delay(10000);
+            Console.WriteLine("10 segundos de atraso");
+        }
+
+        public async Task Task_LongaDuracao()
+        {
+            // espera verificar se é bissexto
+            bool isAnoBissexto = await Task_Tresult_Async();
+
+            Console.WriteLine($"{DateTime.Now.Year} {(isAnoBissexto ? "é" : "não é ")} um ano Bissexto -------- { DateTime.Now}");
+
+            //por ultimo espera 10s e finaliza o longa.
+            await Task_Void_Async();
+
+            //Task taskTResultAsync = Task_Tresult_Async();
+        }
+    }
+}
